089cbd1443907ee9fa695eebf2a567d762cbfd6d
jinah <fiatluxdaisy@gmail.com>
2023-11-16 10:40:23 +0900

    detailpage에 들어가는 single테이블

---
+++ react-project/src/components/SingleTable.jsx
@@ -0,0 +1,50 @@
+import React, { useMemo } from "react";
+import { useTable } from "react-table";
+import { COLUMNS } from "./columns";
+import '../components/SingleTable.css';
+
+export const SingleTable = (props) => {
+
+    const columns = useMemo(() => COLUMNS, []);
+    const data = useMemo(() => props.data, [props.data]);
+
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow
+    } = useTable({
+        columns,
+        data
+    })
+
+    return (
+        <>
+            <table className='single-table' {...getTableProps()}>
+                <thead className="single-table-head">
+                    {headerGroups.map((headerGroup) => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {headerGroup.headers.map((columns) => (
+                                <th className="single-table-th" {...columns.getHeaderProps()}>{columns.render("Header")}</th>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                    {rows.map((row) => {
+                        prepareRow(row);
+                        return (
+                            <tr {...row.getRowProps()}>
+                                {row.cells.map((cell) => {
+                                    return <td className="single-table-td" {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                                })}
+                            </tr>
+                        )
+                    })}
+                </tbody>
+
+            </table>
+        </>
+    )
+};
