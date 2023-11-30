dd99c64369aa212eec84aec7dd77999edab65ca3
jinah <fiatluxdaisy@gmail.com>
2023-11-30 19:23:24 +0900

    patient status modify

--- react-project/src/components/AdminScoreModal.jsx
+++ react-project/src/components/AdminScoreModal.jsx
@@ -3,7 +3,7 @@ import X from '../image/X.png'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const AdminScoreModal = ({closeModal}) => {
+const AdminScoreModal = ({ closeModal }) => {
   const [u_score, setU_score] = useState('');  // state 이름을 sepsis_score에서 u_score로 변경
   const [previousScore, setPreviousScore] = useState(''); // 이전 점수를 저장
 
@@ -34,7 +34,8 @@ const AdminScoreModal = ({closeModal}) => {
       console.log('Response from server:', response.data);
 
       setPreviousScore(u_score); // 새로운 점수를 이전 점수 상태에 저장
-      closeModal(); 
+      localStorage.setItem('confirmedUScore', u_score); // 새로운 점수를 로컬 스토리지에 저장
+      closeModal();
     } catch (error) {
       console.log('패혈증 점수 설정 실패', error);
     }
@@ -42,32 +43,32 @@ const AdminScoreModal = ({closeModal}) => {
 
   return (
     <div className='admin-score-modal'>
-        <div className="x-button-wrap">
-            <button className='x-button' onClick={xIconClick}><img src={X} alt="닫기버튼" className='x-button-img'/></button>
+      <div className="x-button-wrap">
+        <button className='x-button' onClick={xIconClick}><img src={X} alt="닫기버튼" className='x-button-img' /></button>
+      </div>
+      <div className="admin-score-modal-content">
+        <span className='admin-score-modal-title'>패혈증 의심 수치 설정</span>
+        <span className='admin-before-score'>이전 설정 점수 : {previousScore}</span>
+        <div className="modal-score-set">
+          <input
+            type="text"
+            className="score-box"
+            value={u_score}
+            onChange={(e) => {
+              const input = e.target.value;
+              const regex = /^[1-9][0-9]?$|^100$/; // 1부터 100까지의 숫자를 허용하는 정규표현식
+              if (input === "" || regex.test(input)) {
+                setU_score(input);
+              }
+            }}
+          />
+          <span className='score-box-text'>점 이상</span>
         </div>
-        <div className="admin-score-modal-content">
-            <span className='admin-score-modal-title'>패혈증 의심 수치 설정</span>
-            <span className='admin-before-score'>이전 설정 점수 : {previousScore}</span>
-            <div className="modal-score-set">
-            <input
-                type="text"
-                className="score-box"
-                value={u_score}
-                onChange={(e) => {
-                  const input = e.target.value;
-                  const regex = /^[1-9][0-9]?$|^100$/; // 1부터 100까지의 숫자를 허용하는 정규표현식
-                  if (input === "" || regex.test(input)) {
-                    setU_score(input);
-                  }
-                }}
-              />
-                <span className='score-box-text'>점 이상</span>
-            </div>
-            <div>
-              <span className='score-box-text-warn'>1 ~ 100 숫자만 입력 가능</span>
-            </div>
-            <button className='admin-score-modal-confirm' onClick={scoreModalConfirmClick}>확인</button>
+        <div>
+          <span className='score-box-text-warn'>1 ~ 100 숫자만 입력 가능</span>
         </div>
+        <button className='admin-score-modal-confirm' onClick={scoreModalConfirmClick}>확인</button>
+      </div>
     </div>
   )
 }
